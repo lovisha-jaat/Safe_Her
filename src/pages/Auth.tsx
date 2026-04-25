e190c72b8e05367cdd038e0a40485c8c8e60a5e4
lovisha-jaat <lovisha12oct@gmail.com>
2026-04-25 11:15:04 +0530

    Added vercel.json and updated project

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -98,7 +98,7 @@ const Auth = () => {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${window.location.origin}/dashboard`,
+          redirectTo: window.location.origin,
           skipBrowserRedirect: true,
         },
       });
